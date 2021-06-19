4b6f6c6b63b0dbe6a76fe64314e7b33f5779cc02
don-kokone <don_kokone@start.bg>
2021-06-19 23:18:29 +0300

    Fixed script

--- Assets/Javascript/changing-menu-color.js
+++ Assets/Javascript/changing-menu-color.js
@@ -1,25 +1,19 @@
 
 
 const bodyTag = document.querySelector('body');
-const myNav = document.querySelector ('nav') ;
-const htmlTag = document.querySelector ('html') ;
+const myNav = document.querySelector ('header') ;
+
 
 console.log(myNav)
 
 
 let scrolled = () => {
     let dec = scrollY / (bodyTag.scrollHeight - innerHeight);
-    console.log("The dec is: " + dec  * 100)
     return Math.floor(dec * 100)
 }
 
 addEventListener("scroll", () =>{
-  myNav.style.setProperty('background', scrolled() > 50 ? "$color-orange" : "$color-blue");
+  myNav.style.setProperty('background', scrolled() > 10 ? "white" : "transparent");
   
 })
 
-console.log(scrollY);
-console.log(bodyTag.scrollHeight);
-
-
-//
