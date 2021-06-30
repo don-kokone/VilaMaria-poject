6f34b25364d937abf2c8dc7546dd3805c587881b
don-kokone <don_kokone@start.bg>
2021-06-30 21:11:48 +0300

    Commit minor changes about js

--- Assets/Javascript/menu-popup.js
+++ Assets/Javascript/menu-popup.js
@@ -2,14 +2,30 @@ const menu_btn = document.getElementById("menu-button");
 const nav = document.getElementById("navigation");
 const link = document.querySelectorAll("li");
 
+const icons = document.querySelectorAll("button i");
+
+const bars = document.getElementById("bars-icon")
+const close = document.getElementById("close-icon")
+
 menu_btn.addEventListener("click", () =>{
 nav.classList.toggle("active")
+
 })
 
 for (let index = 0; index < link.length; index++) {
     
     link[index].addEventListener("click", () =>{
-        nav.classList.remove("active")
+       nav.classList.remove("active")
+       bars.classList.toggle("active")
+       close.classList.toggle("active")
     })
 }
 
+for (let index = 0; index < icons.length; index++) {
+    
+    icons[index].addEventListener("click", () =>{
+       
+       bars.classList.toggle("active")
+       close.classList.toggle("active")
+    })
+}
