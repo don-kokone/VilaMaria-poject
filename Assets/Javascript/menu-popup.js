ac7e1596bf6aa97de48d34ec835ee3e0a8388078
don-kokone <don_kokone@start.bg>
2021-07-01 22:43:13 +0300

    Commit minor changes

--- Assets/Javascript/menu-popup.js
+++ Assets/Javascript/menu-popup.js
@@ -1,31 +1,38 @@
 const menu_btn = document.getElementById("menu-button");
 const nav = document.getElementById("navigation");
-const link = document.querySelectorAll("li");
-
-const icons = document.querySelectorAll("button i");
+const link = document.querySelectorAll("li a");
 
 const bars = document.getElementById("bars-icon")
 const close = document.getElementById("close-icon")
 
-menu_btn.addEventListener("click", () =>{
-nav.classList.toggle("active")
+let menuOpen = false
+
+menu_btn.addEventListener("click", () => {
+    nav.classList.toggle("active")
 
 })
 
+
 for (let index = 0; index < link.length; index++) {
-    
-    link[index].addEventListener("click", () =>{
-       nav.classList.remove("active")
-       bars.classList.toggle("active")
-       close.classList.toggle("active")
+
+    link[index].addEventListener("click", () => {
+        nav.classList.remove("active")
+        close.classList.toggle("active")
+        bars.classList.toggle("active")
     })
 }
 
-for (let index = 0; index < icons.length; index++) {
-    
-    icons[index].addEventListener("click", () =>{
-       
-       bars.classList.toggle("active")
-       close.classList.toggle("active")
-    })
-}
+
+menu_btn.addEventListener("click", () => {
+    if (!menuOpen) {
+      
+        close.classList.add("active")
+        bars.classList.remove("active")
+        menuOpen = true;
+    }
+    else {
+        close.classList.remove("active")
+        bars.classList.add("active")
+        menuOpen = false
+    }
+})
